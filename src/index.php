963aadfeb9522eba33cc8ce87c7bafdbc094b893
IngvarOlsen <ingvarolsen57@hotmail.com>
2024-05-10 12:23:21 +0200

    rewritten code secured

--- src/index.php
+++ src/index.php
@@ -4,19 +4,13 @@ session_start();
 
 require 'database.php';
 
-if( isset($_SESSION['user_id']) || @$_GET["debug"]=="1"){
+$user = NULL;
 
-	$records = $conn->prepare('SELECT id,email,password FROM users WHERE id = :id');
-	$records->bindParam(':id', $_SESSION['user_id']);
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
-
-	$user = NULL;
-
-	if( count($results) > 0){
-		$user = $results;
-	}
-
+if( isset($_SESSION['user_id']) ){
+    $stmt = $conn->prepare('SELECT id, email FROM users WHERE id = :id');
+    $stmt->bindParam(':id', $_SESSION['user_id']);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -24,30 +18,30 @@ if( isset($_SESSION['user_id']) || @$_GET["debug"]=="1"){
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome to your Web App</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+    <title>Welcome to Your Web App</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-	<div class="header">
-		<a href="/php-login">Vulnerable Login</a>
-	</div>
+    <div class="header">
+        <a href="/php-login">Your Web App</a>
+    </div>
 
-	<?php if( !empty($user) ): ?>
+    <?php if( $user ): ?>
 
-		<br />Welcome <?= $user['email']; ?> 
-		<br /><br />You are successfully logged in!
-		<br /><br />
-		<a href="logout.php">Logout?</a>
+        <br />Welcome <?= htmlspecialchars($user['email']); ?>
+        <br /><br />You are successfully logged in!
+        <br /><br />
+        <a href="logout.php">Logout?</a>
 
-	<?php else: ?>
+    <?php else: ?>
 
-		<h1>Please Login or Register</h1>
-		<a href="login.php">Login</a> or
-		<a href="register.php">Register</a>
+        <h1>Please Login or Register</h1>
+        <a href="login.php">Login</a> or
+        <a href="register.php">Register</a>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
 </body>
 </html>
